1dc517176ddb23f28ddbcdf1b138afd423c9140f
UlviNuri <102859099+UlviNuri@users.noreply.github.com>
2022-08-16 19:03:15 +0400

    new

---
+++ assest/js/app.js
@@ -0,0 +1,21 @@
+const burger = document.querySelector(".nav_icon");
+
+if (burger) {
+  burger.addEventListener("click", function () {
+    const navmenu = document.querySelector(".nav_left");
+    console.log(navmenu);
+
+    if (navmenu.style.transform === "translateY(-500px)") {
+      navmenu.style.transform == "translateX(0px)";
+    } else {
+      navmenu.style.transform = "translateX(-500px)";
+    }
+  });
+}
+
+
+var loader = document.getElementById("loader");
+window.addEventListener("load", reload);
+function reload() {
+  loader.classList.add("dispear");
+}
